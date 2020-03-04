1f33ad368d7081f528268ba47cb347adcc1ce895
haiyang.shen <shy74364249264>
2020-03-04 16:43:23 +0800

    first commit

---
+++ src/views/3DMap/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>三维地图</div>
+</template>
+
+<script>
+  export default {
+    name: "three-dMap"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/logging/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Button @click="throwError">Default</Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "logging",
+    methods: {
+      throwError(){
+        this.run();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
